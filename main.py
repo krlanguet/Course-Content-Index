0c35dacd823b3b5a6c0d6285abf0cbda8f27d3e7
Keith R Languet <krlanguet@gmail.com>
2018-07-02 16:30:09 -0500

    Working course creation!

--- main.py
+++ main.py
@@ -22,15 +22,16 @@ def init():
     # Copies entire contents of file into string
     with open('courses.json', 'r+') as data_file:
         course_list = loads(data_file.read())
+        # Potential bug here !! need to create empty list if there is no file!
 
     for i, entry in enumerate(course_list):
-        course_list[i] = course(entry)
+        course_list[i] = course(dict_from_json = entry)
 
     return course_list
 
 
 # Function for saving to the databse and exiting after all commands are finished
-def shutdown():
+def shutdown(course_list):
     # Dumps potentially modified string back into file
     with open('courses.json', 'w+') as data_file:
         data_file.write('[\n')
@@ -43,23 +44,29 @@ def shutdown():
 
 
 class course:
-    def __init__(self, dict_from_json):
-        d = dict_from_json
-        self.name = d["name"]
-        self.descriptions = d["descriptions"]
-        #self.resources = {}
+    def __init__(self, dict_from_json = None, name=None, descriptions=None):
+        if dict_from_json:
+            d = dict_from_json
+            self.name = d["name"]
+            self.descriptions = d["descriptions"]
+            #self.resources = {}
+        else:
+            self.name = name
+            self.descriptions = descriptions
 
 
 @click.command()
 @click.argument('name')
 @click.option('--description', '-d', type=(str, str), default=(), help='Course description and source', multiple=True)
 def new_course(name, description):
-    # Multiple descriptions creates a single description tuple containing multiple inputs tuples
-    print(name)
-    print(description)
-
-
-if __name__ == '__main__':
     course_list = init()
-    #new_course()
-    shutdown()
+
+    # Multiple descriptions creates a single description tuple containing multiple inputs tuples
+    descriptions = []
+    for d in description:
+        descriptions.append({'text': d[0], 'source': d[1]})
+
+
+    course_list.append(course(name=name, descriptions=descriptions))
+
+    shutdown(course_list)
