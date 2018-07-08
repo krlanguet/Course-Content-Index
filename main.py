d916da8136f9ca3e06946f42512c70e259a16b2c
Keith R Languet <krlanguet@gmail.com>
2018-07-08 18:36:24 -0500

    Last attempt to improve efficiency

--- main.py
+++ main.py
@@ -16,15 +16,23 @@ from time import strftime
 from shutil import copy
 
 
-# Function for opening the database and preparing for any commands
-def init():
-    '''
-    t = strftime('%H:%M:%S') # Local time in 24Hour, minute, seconds
+class course:
+    def __init__(self, dict_from_json = None, name=None, descriptions=None):
+        if dict_from_json:
+            d = dict_from_json
+            self.name = d["name"]
+            self.descriptions = d["descriptions"]
+            #self.resources = {}
+        else:
+            self.name = name
+            self.descriptions = descriptions
 
-    # Backup previous data output
-    if isfile('courses.json'):
-        copy('courses.json', 'data_backup/' + t + '.json')
-    '''
+
+@click.command()
+@click.argument('name')
+@click.option('--description', '-d', type=(str, str), default=(), help='Course description and source', multiple=True)
+def new_course(name, description):
+    start_time = time()
 
     connection = connect("courses.db")
 
@@ -46,42 +54,16 @@ def init():
         );
     """)
 
-    return [connection, cursor]
 
-
-# Function for saving to the databse and exiting after all commands are finished
-def shutdown(connection):
-    connection.commit()
-    connection.close()
-
-
-class course:
-    def __init__(self, dict_from_json = None, name=None, descriptions=None):
-        if dict_from_json:
-            d = dict_from_json
-            self.name = d["name"]
-            self.descriptions = d["descriptions"]
-            #self.resources = {}
-        else:
-            self.name = name
-            self.descriptions = descriptions
-
-
-@click.command()
-@click.argument('name')
-@click.option('--description', '-d', type=(str, str), default=(), help='Course description and source', multiple=True)
-def new_course(name, description):
-    start_time = time()
-
-    con, curs = init()
-
-    curs.execute("INSERT INTO courses (name) VALUES ('{}')".format(name))
-    course_id = curs.lastrowid
+    cursor.execute("INSERT INTO courses (name) VALUES ('{}')".format(name))
+    course_id = cursor.lastrowid
     
     # Multiple descriptions creates a single description tuple containing multiple inputs tuples
     for d in description:
-        curs.execute("INSERT INTO descriptions (course_id, text, source) VALUES ({}, '{}', '{}')".format(course_id, d[0], d[1]))
+        cursor.execute("INSERT INTO descriptions (course_id, text, source) VALUES ({}, '{}', '{}')".format(course_id, d[0], d[1]))
 
 
-    shutdown(con)
+    connection.commit()
+    connection.close()
+
     print("Total time: {} seconds".format(time() - start_time))
